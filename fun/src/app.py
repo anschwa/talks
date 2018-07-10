3bd384f69ec06041de5e3309f1938086746c7dda
Adam Schwartz <adam@anschwa.com>
2018-07-10 17:38:03 -0400

    add talk

---
+++ fun/src/app.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+import bottle
+
+
+@bottle.route('/')
+def hello():
+    return "<h1>What is six times nine?</h1>"
+
+
+bottle.run(host='localhost', port=8080, debug=True)
