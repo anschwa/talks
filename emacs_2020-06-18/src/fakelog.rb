13e34ddbee38da2585fbe1cc113807a880dc81f5
Adam Schwartz <adam@anschwa.com>
2020-06-17 23:56:35 -0400

    update why emacs

--- emacs_2020-06-18/src/fakelog.rb
+++ emacs_2020-06-18/src/fakelog.rb
@@ -1,10 +1,10 @@
 #!/bin/ruby
 # Generate a fake log file that looks interesting
 # Such as:
-# ~> ruby fakelog.rb 3
-# [Wed, May 06 18:23:45-0400 2020] WARN [worker 046] [client 254.201.103.230 Mozilla/5.0 (Windows; U; Win 9x 4.90; SG; rv:1.9.2.4) Gecko/20101104 Netscape/9.1.0285] Hacking auxiliary matrix...
-# [Sat, May 16 11:27:38-0400 2020] DEBU [worker 073] [client 26.175.169.195 Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16] Bypassing neural protocol...
-# [Fri, Apr 24 18:16:41-0400 2020] ERRO [worker 021] [client 102.195.202.68 Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko] Error overriding interface
+# ~> ruby fakelog.rb 3 | sort --stable --keys"1,1"
+# 2020-05-27T11:12:49-04:00 [WARN] [worker 046] [client 254.201.103.230 Mozilla/5.0 (Windows; U; Win 9x 4.90; SG; rv:1.9.2.4) Gecko/20101104 Netscape/9.1.0285] Hacking auxiliary matrix...
+# 2020-05-29T13:23:25-04:00 [DEBU] [worker 073] [client 26.175.169.195 Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16] Bypassing neural protocol...
+# 2020-05-31T00:56:47-04:00 [ERRO] [worker 021] [client 102.195.202.68 Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko] Error overriding interface
 
 require 'faker'
 
@@ -17,7 +17,7 @@ def log
   date = Faker::Time.between(
     from: DateTime.now - 30,
     to: DateTime.now
-  ).strftime("%a, %b %e %H:%M:%S%z %Y")
+  ).iso8601
 
   msg =
     if status == "ERRO"
@@ -26,7 +26,7 @@ def log
       "#{Faker::Hacker.ingverb.capitalize} #{Faker::Hacker.adjective} #{Faker::Hacker.noun}"
     end
 
-  "[#{date}] #{status} [worker #{code}] [client #{ip} #{useragent}] #{msg}"
+  "#{date} [#{status}] [worker #{code}] [client #{ip} #{useragent}] #{msg}"
 end
 
 # Output n log lines to stdout
